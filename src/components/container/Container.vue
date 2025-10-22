d0471619a8568f8e357e66187d394e649c3bc04d
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-10-23 05:58:08 +0700

    feat: container component

---
+++ src/components/container/Container.vue
@@ -0,0 +1,29 @@
+<script setup>
+import { computed } from 'vue';
+import ContainerConfig from './Container.config';
+
+const props = defineProps({
+  maxScreen: {
+    type: String,
+    validator: (val) => ['sm', 'md', 'lg', 'xl'].includes(val),
+  },
+  tag: {
+    type: String,
+    default: 'div',
+  },
+});
+
+const containerClass = computed(() => {
+  if (!props.maxScreen) {
+    return '';
+  }
+
+  return ContainerConfig.maxScreenBreakpoints[props.maxScreen];
+});
+</script>
+
+<template>
+  <component :is="tag" :class="[ContainerConfig.baseClass, containerClass]">
+    <slot />
+  </component>
+</template>
