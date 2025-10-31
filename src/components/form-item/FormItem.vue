3d61fdb2e15bcfe524b7ada753a261ec755e0204
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-10-31 18:46:57 +0700

    feat: form item component

---
+++ src/components/form-item/FormItem.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  id: {
+    type: String,
+    required: true,
+  },
+  label: {
+    type: String,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <div class="flex flex-col gap-y-1">
+    <label :for="id">{{ label }}</label>
+    <slot :id="id" />
+  </div>
+</template>
