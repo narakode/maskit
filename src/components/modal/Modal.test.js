c9cdecdf7f3dd1636ec29637929bb820a801fb1b
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-11-06 20:57:16 +0700

    feat: modal action

--- src/components/modal/Modal.test.js
+++ src/components/modal/Modal.test.js
@@ -5,6 +5,7 @@ import vClickOutside from 'click-outside-vue3';
 import { MotionPlugin } from '@vueuse/motion';
 import Container from '../container/Container.vue';
 import Card from '../card/Card.vue';
+import { Icon } from '@iconify/vue';
 
 describe.only('Modal', () => {
   test('should not render content by default', () => {
@@ -63,5 +64,58 @@ describe.only('Modal', () => {
 
     expect(card.exists()).toBe(true);
     expect(card.props('bordered')).toBe(false);
+  });
+
+  test('should render title', () => {
+    const wrapper = mount(Modal, {
+      props: {
+        visible: true,
+        title: 'Test',
+      },
+      global: {
+        plugins: [vClickOutside, MotionPlugin],
+      },
+    });
+
+    const card = wrapper.findComponent(Card);
+
+    expect(card.props('title')).toEqual('Test');
+  });
+
+  test('should render close button', () => {
+    const wrapper = mount(Modal, {
+      props: {
+        visible: true,
+        title: 'Test',
+      },
+      global: {
+        plugins: [vClickOutside, MotionPlugin],
+      },
+    });
+
+    const card = wrapper.findComponent(Card);
+    const icon = card.findComponent(Icon);
+
+    expect(icon.exists()).toBe(true);
+    expect(icon.props('icon')).toEqual('tabler:x');
+  });
+
+  test('should hide modal when close button clicked', async () => {
+    const wrapper = mount(Modal, {
+      props: {
+        visible: true,
+        title: 'Test',
+        'onUpdate:visible': (e) => wrapper.setProps({ visible: e }),
+      },
+      global: {
+        plugins: [vClickOutside, MotionPlugin],
+      },
+    });
+
+    const closeButton = wrapper.find('button.cursor-pointer.text-gray-900');
+
+    await closeButton.trigger('click');
+
+    expect(wrapper.props('visible')).toBe(false);
   });
 });
