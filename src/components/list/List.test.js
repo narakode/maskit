422fdbe6bc04b794d8bcaae27f0f3895aa9f1360
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-11-08 17:37:45 +0700

    test: list slots

---
+++ src/components/list/List.test.js
@@ -0,0 +1,71 @@
+import { mount } from '@vue/test-utils';
+import { describe, expect, test } from 'vitest';
+import List from './List.vue';
+import Card from '../card/Card.vue';
+
+describe.only('List', () => {
+  test('should render paddless Card', () => {
+    const wrapper = mount(List);
+
+    const card = wrapper.findComponent(Card);
+
+    expect(card.exists()).toBe(true);
+    expect(card.props('paddless')).toBe(true);
+  });
+
+  test('should render empty message on empty data', () => {
+    const wrapper = mount(List);
+
+    expect(wrapper.text()).toContain("There's nothing here yet.");
+  });
+
+  test('should render default slot', () => {
+    const wrapper = mount(List, {
+      props: {
+        data: [{ id: 1, name: 'test' }],
+      },
+      slots: {
+        default: `<template #default><p>Test</p></template>`,
+      },
+    });
+
+    expect(wrapper.html()).toContain('<p>Test</p>');
+  });
+
+  test('should render item slot', () => {
+    const wrapper = mount(List, {
+      props: {
+        data: [
+          { id: 1, name: 'item 1' },
+          { id: 2, name: 'item 2' },
+        ],
+      },
+      slots: {
+        item: `<template #item="{ item }"><p>{{item.name}}</p></template>`,
+      },
+    });
+
+    expect(wrapper.html()).toContain('<p>item 1</p>');
+    expect(wrapper.html()).toContain('<p>item 2</p>');
+  });
+
+  test('should render header slot', () => {
+    const wrapper = mount(List, {
+      slots: {
+        header: `<template #header><p>test</p></template>`,
+      },
+    });
+
+    expect(wrapper.html()).toContain('<p>test</p>');
+  });
+
+  test('should render footer slot', () => {
+    const wrapper = mount(List, {
+      slots: {
+        footer: `<template #footer><p>test</p></template>`,
+      },
+    });
+
+    expect(wrapper.html()).toContain('<p>test</p>');
+  });
+});
---
+++ src/components/list/List.vue
@@ -0,0 +1,57 @@
+<script setup>
+import { computed } from 'vue';
+import Card from '../card/Card.vue';
+
+const props = defineProps({
+  data: {
+    type: Array,
+    default: () => [],
+  },
+  size: {
+    type: String,
+    default: 'md',
+    validator: (value) => ['sm', 'md', 'lg'].includes(value),
+  },
+  striped: {
+    type: Boolean,
+    default: true,
+  },
+  hoverable: Boolean,
+  emptyMessage: {
+    type: String,
+    default: "There's nothing here yet.",
+  },
+});
+
+const classes = computed(() => {
+  const itemSizes = {
+    sm: 'px-3 py-2',
+    md: 'p-4',
+    lg: 'p-6',
+  };
+
+  return {
+    item: [itemSizes[props.size], props.hoverable ? 'hover:bg-gray-50' : ''],
+  };
+});
+</script>
+
+<template>
+  <Card paddless>
+    <div :class="[striped ? 'divide-y divide-gray-300' : '']">
+      <slot name="header" :classes="{ item: classes.item }" />
+      <div
+        v-if="!data.length"
+        :class="[classes.item, 'text-gray-700 text-center']"
+      >
+        {{ emptyMessage }}
+      </div>
+      <slot :classes="{ item: classes.item }">
+        <div v-for="(item, index) in data" :key="item.id" :class="classes.item">
+          <slot name="item" :index="index" :item="item" />
+        </div>
+      </slot>
+      <slot name="footer" :classes="{ item: classes.item }" />
+    </div>
+  </Card>
+</template>
