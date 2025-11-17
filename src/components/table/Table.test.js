a7ee10693da0f6036eadfc95dc32506e53abebae
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-11-17 20:56:35 +0700

    feat: table component

---
+++ src/components/table/Table.test.js
@@ -0,0 +1,15 @@
+import { describe, test } from 'vitest';
+
+describe('Table', () => {
+  test.skip('should render table');
+  test.skip('should render thead');
+  test.skip('should render columns');
+  test.skip('should render loading row when loading is true');
+  test.skip('should render empty merrage row when item is empty');
+  test.skip('should render item rows');
+  test.skip('should not have hover class on item row');
+  test.skip('should have hover class on item row when hoverable is true');
+  test.skip('should not render tfoot by default');
+  test.skip('should render tfoot when footer slot is exists');
+  test.skip('should render footer slot');
+});
