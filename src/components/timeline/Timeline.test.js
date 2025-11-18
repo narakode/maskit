854ab5fd0bb8736ee969a99d8133635b18fbb964
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-11-18 14:51:55 +0700

    feat: timeline component

---
+++ src/components/timeline/Timeline.test.js
@@ -0,0 +1,9 @@
+import { describe, test } from 'vitest';
+
+describe('Timeline', () => {
+  test.skip('should render list container');
+  test.skip('should render list item status');
+  test.skip('should render list item time');
+  test.skip('should render list item name');
+  test.skip('should render list item text');
+});
---
+++ src/components/timeline/Timeline.vue
@@ -0,0 +1,30 @@
+<script setup>
+defineProps({
+  items: {
+    type: Array,
+    required: true,
+  },
+});
+</script>
+
+<template>
+  <ol class="relative border-s border-gray-200 space-y-10">
+    <li v-for="(item, index) in items" :key="item.id" class="ms-4">
+      <div
+        :class="[
+          'absolute w-3 h-3 rounded-full mt-1.5 -start-1.5 border border-white',
+          index === 0 ? 'bg-blue-600' : 'bg-gray-200',
+        ]"
+      />
+      <time class="mb-1 text-sm font-normal leading-none text-gray-400">
+        {{ item.time }}</time
+      >
+      <p class="font-bold text-gray-900 lg:text-lg">
+        {{ item.name }}
+      </p>
+      <p v-if="item.text" class="text-base font-normal text-gray-500">
+        {{ item.text }}
+      </p>
+    </li>
+  </ol>
+</template>
